ba11afcab75c888507b21a0d0c9353e3d719763f
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-06-23 14:45:06 +0530

    final be

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,44 @@
-import { WebSocketServer, WebSocket} from "ws";
-const wss = new WebSocketServer({port:8080});
+import { WebSocketServer, WebSocket } from "ws";
 
-let userCount =0;
-let allSockets:WebSocket[]=[];
-wss.on("connection",(socket)=>{
-    allSockets.push(socket);
-    console.log("user connected "+userCount);
-    userCount=userCount+1;
-    socket.on("message",(event)=>{
-        console.log("message received "+event.toString());
-        for(let i =0 ; i < allSockets.length;i++){
-            const s = allSockets[i];
-            s.send(event.toString()+":sent from the server");
+const wss = new WebSocketServer({ port: 8080 });
+
+interface User {
+    socket: WebSocket;
+    room: string;
+}
+
+let allSockets: User[] = [];
+
+wss.on("connection", (socket) => {
+
+    socket.on("message", (message) => {
+        // @ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type == "join") {
+            console.log("user joined room " + parsedMessage.payload.roomId);
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
         }
+
+        if (parsedMessage.type == "chat") {
+            console.log("user wants to chat");
+            // const currentUserRoom = allSockets.find((x) => x.socket == socket).room
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].socket == socket) {
+                    currentUserRoom = allSockets[i].room
+                }
+            }
+
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].room == currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message)
+                }
+            }
+        }
+
     })
 
 })
-
-
