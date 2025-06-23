7957d577219192e63c57e6ed1e2a0770a24a1e89
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-06-23 12:09:14 +0530

    basic setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-import { WebSocketServer} from "ws";
+import { WebSocketServer, WebSocket} from "ws";
 const wss = new WebSocketServer({port:8080});
 
 let userCount =0;
+let allSockets:WebSocket[]=[];
 wss.on("connection",(socket)=>{
+    allSockets.push(socket);
     console.log("user connected "+userCount);
     userCount=userCount+1;
+    socket.on("message",(event)=>{
+        console.log("message received "+event.toString());
+        for(let i =0 ; i < allSockets.length;i++){
+            const s = allSockets[i];
+            s.send(event.toString()+":sent from the server");
+        }
+    })
+
 })
 
 
